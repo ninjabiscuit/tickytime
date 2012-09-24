443a2a8e5dafbd4334013c5a3020161846d3d22a
Andrew Walker <andy@madebymany.co.uk>
2012-09-24 20:46:05 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickytime::Application.config.secret_token = 'fb408bb1233997026b75c53f08cf7a5b9e96e08b4f0ca75a0c294cb3cb2ee455ed6908500140d9b8894bc274a1d171d8afe6a3078c23d581219888fcdc54fba5'
