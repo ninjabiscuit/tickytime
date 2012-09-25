a302e24633f3e07087cc9f937ac1621e9eea79b4
Andrew Walker <andy@madebymany.co.uk>
2012-09-25 22:51:07 +0100

    got the upload working

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,21 +2,26 @@ class CalendarController < ApplicationController
 
   before_filter :require_oauthed_user!
 
-  def import
+  require 'csv'
+
+  def upload
     if request.post? && params[:file].present?
-      infile = params[:file].read
+      
+      csv_text = params[:file].read
+      csv_text = csv_text.gsub("\"", "")
 
-      csv = CSV.parse(infile, :headers => true)
+      puts csv_text
+
+      csv = CSV.parse(csv_text, :headers => true)
       csv.each_with_index do |row, i|
-
+      
         # SKIP: header i.e. first row OR blank row
-        next if i == 0 || row.join.blank?
+        next if i == 0
 
         row = row.to_hash.with_indifferent_access
         puts row.to_hash.symbolize_keys
-        #Moulding.create!(row.to_hash.symbolize_keys)
 
-      end
+      end  
     end
   end
 
